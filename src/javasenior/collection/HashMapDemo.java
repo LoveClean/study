658436f6b5c24c6f3d9e0b43de28ac20bf034b03
LoveClean <641655770@qq.com>
2019-08-15 16:52:14 +0800

    HashMap

---
+++ src/javasenior/collection/HashMapDemo.java
@@ -0,0 +1,40 @@
+package javasenior.collection;
+
+import java.util.*;
+
+/**
+ * HashMap
+ *
+ * @author huangpengfei
+ * @version 1.0
+ * @date 2019/8/15 16:45
+ */
+public class HashMapDemo {
+    public static void main(String[] args) {
+        Map map = new HashMap();
+        map.put("AA", 123);
+        map.put(45, 123);
+        map.put("BB", 56);
+
+        // 遍历key
+        Set keySet = map.keySet();
+        Iterator iterator = keySet.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        // 遍历value
+        Collection values = map.values();
+        Iterator iterator2 = values.iterator();
+        while (iterator2.hasNext()) {
+            System.out.println(iterator2.next());
+        }
+
+        // 遍历entry
+        Set entrySet = map.entrySet();
+        Iterator iterator3 = entrySet.iterator();
+        while (iterator3.hasNext()) {
+            System.out.println(iterator3.next());
+        }
+    }
+}
